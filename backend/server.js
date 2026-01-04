f05eaecd3d3c8c2c47a748c40500987e49ab6fc5
naveen <pnrreddy84@gmail.com>
2026-01-04 23:20:04 +0530

    Commmitt

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const cors = require("cors")
 const db = require("./db")
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json())
 
 // Signup
